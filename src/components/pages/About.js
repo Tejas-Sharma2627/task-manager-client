41955ee13090c98c6f07e66a6ff4a6f1384c4cee
Tejas Sharma <sharma.tejas4776@gmail.com>
2023-07-21 21:28:04 +0530

    message

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -5,7 +5,7 @@ const About = () => {
     <div className="container">
       <div className="mt-5">
         <h1 className="mb-2">About</h1>
-        <h3 className="mb-5 text-primary">Created by: Siddharth Sharma</h3>
+        <h3 className="mb-5 text-primary">Created by: Tejas Sharma</h3>
         <h5 className="text-success">
           This <strong className="text-dark">Task Manager</strong> web app uses
           a REST API (created by me) to manage the user profiles and their
